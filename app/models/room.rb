334159f5ab963afa57f833c766b810f6a23c7a63
jgoodidea <gloproman0211@gmail.com>
2022-11-13 09:38:34 -0800

    room, user template channel list

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+    validates_uniqueness_of :name
+    scope :public_rooms, -> { where(is_private: false) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :rooms
   root 'pages#home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get 'users', to: 'devise/sessions#new'
   end
+  get 'user/:id', to: 'users#show', as: 'user'
 
   # Defines the root path route ("/")
   # root "articles#index"
