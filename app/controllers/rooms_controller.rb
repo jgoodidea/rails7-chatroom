c6e685d970f94ae8e68d8d722d79a409c61c96a1
jgoodidea <gloproman0211@gmail.com>
2022-11-13 10:05:26 -0800

    turbo_stream

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,13 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @room = Room.new
     @rooms = Room.public_rooms
+
     @users = User.all_except(current_user)
+  end
+
+  def create
+    @room = Room.create(name: params["room"]["name"])
   end
 end
