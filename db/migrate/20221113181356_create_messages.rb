5b04f2c387da2b99f7a1ef35e5d5e73f893aa059
jgoodidea <gloproman0211@gmail.com>
2022-11-13 10:52:20 -0800

    fix message model creation issue

--- db/migrate/20221113181356_create_messages.rb
+++ db/migrate/20221113181356_create_messages.rb
@@ -2,8 +2,7 @@ class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :room
-      t.string :references
+      t.references :room, null: false, foreign_key: true
       t.text :body
 
       t.timestamps
