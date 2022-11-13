5605de44326be1decc9aeeb699db9f84e259e696
jgoodidea <gloproman0211@gmail.com>
2022-11-13 10:23:41 -0800

    fix wrong line on index function add show case

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,9 +5,21 @@ class RoomsController < ApplicationController
     @room = Room.new
     @rooms = Room.public_rooms
 
+    @users = User.all_except(current_user)
+    render 'index'
+  end
+
+  def show
     @single_room = Room.find(params[:id])
 
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
     @users = User.all_except(current_user)
+    render 'index'
   end
 
   def create
